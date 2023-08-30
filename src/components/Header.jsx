5d6b1201600eaa27bae6574ee8d91fccc4c97b38
mattsahr <mattsahr@gmail.com>
2023-08-30 15:23:48 +0100

    Simplify CSS styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header({ handleAddTodo }) {
       <div className='container text-center'>
         <div className="d-flex align-items-center justify-content-center">
           <img className='mb-2' src={ReactSVG} alt="React SVG" />
-          <h3 className='py-3 mx-2'>Todo App: React & Bootstrap</h3>
+          <h3 className='py-3 mx-2'>Todo!</h3>
           <button className='btn btn-primary mx-2' data-bs-toggle="collapse" data-bs-target='#create-todo' onClick={
             e => e.target.innerText = e.target.getAttribute('aria-expanded') == "true" ? 'Close Menu' : 'Create Todo'}
           >Create Todo</button>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,17 +1,33 @@
 import React, { useId } from 'react';
 
 function TodoItem({ todo, handleTodoChecked, handleTodoRemove }) {
-  const id = useId();
 
-  return (
-    <div className='TodoItem container text-center w-75 rounded-2 py-3 my-2 text-white d-flex justify-content-between' style={{ backgroundColor: 'var(--accent-color' }}>
-      <div className='input-group d-flex align-items-center' role='group'>
-        <input className='form-check-input rounded-1' type='checkbox' id={`${id}-checkbox`} checked={todo.completed} onChange={() => handleTodoChecked(todo.id, !todo.completed)} />
-        <label for={`${id}-checkbox`}><h4 className='m-2' style={{ fontWeight: 'normal' }}>{todo.text}</h4></label>
-      </div>
-      <button className='btn-close btn-close-white align-self-center' onClick={() => handleTodoRemove(todo.id)}></button>
-    </div>
-  );
+    const id = useId();
+
+    return (
+        <div className="todo-wrapper">
+
+            <div className="todo-item">
+
+                <input className="form-check-input rounded-1" 
+                    type="checkbox"
+                    id={`${id}-checkbox`} 
+                    checked={todo.completed} 
+                    onChange={() => handleTodoChecked(todo.id, !todo.completed)} />
+                
+                <label htmlFor={`${id}-checkbox`}>
+                    <h4 className="m-2" style={{ fontWeight: 'normal' }}>
+                        {todo.text}
+                    </h4>
+                </label>
+
+            </div>
+
+            <button className="close-button"
+                onClick={() => handleTodoRemove(todo.id)}>X</button>
+
+        </div>
+    );
 }
 
 export default TodoItem;
